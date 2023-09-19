4625d878a047c30046e0409e38bd34ab51f862b6
gustavobruder <gugabruder@gmail.com>
2023-09-18 23:59:34 -0300

    adicionado pontuação do agente objetivo

--- agente_objetivo.py
+++ agente_objetivo.py
@@ -31,9 +31,12 @@ def gerar_caminho_objetivo():
 def exibir_caminho_objetivo(matriz):
     global posAPAx
     global posAPAy
+    global pontos
 
     posAPAx = 1
     posAPAy = 1
+    pontos = -1  # comeca em -1 pois o inicio do caminho (1,1) deve ser desconsiderado
+
     caminho_dijkstra = Grafo((pegar_vertices(matriz))).definir_menor_caminho()
     caminho_objetivo = definir_caminho(caminho_dijkstra)
 
@@ -41,12 +44,16 @@ def exibir_caminho_objetivo(matriz):
         for (x, y) in caminho_objetivo:
             posAPAx = x
             posAPAy = y
+            pontos += 1
 
             exibir_matriz(matriz)
 
             if matriz[posAPAx][posAPAy] == 2:
                 matriz[posAPAx][posAPAy] = 0
+                pontos += 1
                 exibir_matriz(matriz)
+
+    print(f'Pontos -> {pontos}')
 
 
 def exibir_matriz(matriz):
